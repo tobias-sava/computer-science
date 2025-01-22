3b7560a3d82199514b13669fea313d4b0edf003b
tobias-sava <tobias.svgb@gmail.com>
2025-01-22 19:02:11 +0200

    push

---
+++ MIT 6.100L - Intro to CS/Lecture 1/Problem Sets/PSET0.py
@@ -0,0 +1,20 @@
+# Problem Set 0
+# Name: Tobias Sava
+# Date: 1/22/2025
+# Time Spent: 10 seconds? xd
+
+# 1. At the top of your file and type: import numpy
+# 2. Now write a line that sets a variable named x to 5.
+# 3. Now write a line that sets a variable named y to 8.
+# 4. Add variables x and y, and save the result to a variable named z.
+# 5. Now save the result of this command: numpy.log2(z) to a variable named a.
+
+import numpy
+
+x = 5
+y = 8
+
+z = x + y
+
+a = numpy.log2(z)
+
