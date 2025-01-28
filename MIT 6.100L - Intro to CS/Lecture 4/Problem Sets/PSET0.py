82f1dc1eec0ad118a79ff5bd7282b106f941225e
tobias-sava <tobias.svgb@gmail.com>
2025-01-28 16:35:13 +0200

    push

---
+++ MIT 6.100L - Intro to CS/Lecture 4/Problem Sets/PSET0.py
@@ -0,0 +1,11 @@
+# Problem Set 0
+# Name: Tobias Sava
+# Date: 28/01/2025
+# Time Spent: NULL
+
+# Assume you are given a positive integer variable named N. 
+# Write a piece of Python code that finds the cube root of N. 
+# The code prints the cube root if N is a perfect cube or it prints 
+# error if N is not a perfect cube. Hint: use a loop that increments a 
+# counter—you decide when the counter should stop.
+
