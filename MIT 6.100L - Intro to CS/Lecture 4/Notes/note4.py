eaf4abbaa920f22480361059eabfdd5e85325450
tobias-sava <tobias.svgb@gmail.com>
2025-02-24 14:11:59 +0200

    guess whos back

--- MIT 6.100L - Intro to CS/Lecture 4/Notes/note4.py
+++ MIT 6.100L - Intro to CS/Lecture 4/Notes/note4.py
@@ -1,4 +1,4 @@
-# 28/05/2025
+# 24/02/2025
 
 # guess and check algorithm part 2
 
@@ -9,3 +9,17 @@
 # write a program that checks through all the numbers from 1 to 10 and prints the secret value if its in that range
 
 # continue from 44:18 on yt vid
+
+secret = 9 # harcoding secret number
+numfound = False # making a flag (boolean variable) to trigger not found print statement if number within range in not found
+
+for snum in range (1, 11): # for the secret number in range 1 through 10
+    if snum == secret: # if snum (1, 2, 3 etc.) is equal to our secret number:
+        print(snum + 'found') # printing result
+        numfound = True # number has been found
+    if numfound != True: # if number is not found:
+        print('number not found, get better')
+
+# alternate way of stating if snum is not found
+    if not numfound:
+        print('number not found')
--- MIT 6.100L - Intro to CS/Lecture 4/Problem Sets/PSET0.py
+++ MIT 6.100L - Intro to CS/Lecture 4/Problem Sets/PSET0.py
@@ -3,9 +3,12 @@
 # Date: 28/01/2025
 # Time Spent: NULL
 
-# Assume you are given a positive integer variable named N. 
-# Write a piece of Python code that finds the cube root of N. 
-# The code prints the cube root if N is a perfect cube or it prints 
-# error if N is not a perfect cube. Hint: use a loop that increments a 
+# Assume you are given a positive integer variable named N.
+# Write a piece of Python code that finds the cube root of N.
+# The code prints the cube root if N is a perfect cube or it prints
+# error if N is not a perfect cube. Hint: use a loop that increments a
 # counter—you decide when the counter should stop.
 
+n = input("Enter a positive integer: ")
+
+for
