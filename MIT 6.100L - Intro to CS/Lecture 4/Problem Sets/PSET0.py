2e99b47d515a2ad42a7240fab4aeaee9481a3238
tobias-sava <tobias.svgb@gmail.com>
2025-02-24 14:47:15 +0200

    pset0 finished

--- MIT 6.100L - Intro to CS/Lecture 4/Problem Sets/PSET0.py
+++ MIT 6.100L - Intro to CS/Lecture 4/Problem Sets/PSET0.py
@@ -3,12 +3,18 @@
 # Date: 28/01/2025
 # Time Spent: NULL
 
-# Assume you are given a positive integer variable named N.
-# Write a piece of Python code that finds the cube root of N.
-# The code prints the cube root if N is a perfect cube or it prints
-# error if N is not a perfect cube. Hint: use a loop that increments a
-# counter—you decide when the counter should stop.
+# Assume you are given a positive integer variable named Cube.
+# Write a piece of Python code that finds the cube root of Cube.
+# The code prints the cube root if Cube is a perfect cube or it prints error if Cube is not a perfect cube.
 
-n = input("Enter a positive integer: ")
+cube = int(input("Enter a positive integer: ")) # collecting int from user and storing in variable
 
-for
+for guess in range(abs(cube)+1): # loops through the absolute cube + 1 (+1 is added to skip 0 cube error)
+    if guess ** 3 >= abs(cube): # if the guess cubed is bigger or equal to abs(user input)
+        break # breaking here
+if guess ** 3 != abs(cube): # if guess cubed does not equal user input
+    print(cube, "is not a perfect cube") # prints result (non cube)
+else:
+    if cube < 0: # if cube is negative
+        guess = -guess # since positive/negative roots are the same, im handling negative number scenarios by simply making the guess negative
+    print("Cube root of" + str(cube) + " is " + str(guess)) # prints result (cube)
