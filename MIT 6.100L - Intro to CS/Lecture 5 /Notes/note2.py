a24ab05c5e994e8b992d7a03d532d125b9a1c9a7
tobias-sava <tobias.svgb@gmail.com>
2025-02-26 13:45:53 +0200

    in progress

--- MIT 6.100L - Intro to CS/Lecture 5 /Notes/note2.py
+++ MIT 6.100L - Intro to CS/Lecture 5 /Notes/note2.py
@@ -1,3 +1,14 @@
 # 26/02/2025
 
-# filler
+# Converting a float into binary
+
+num =
+result = ''
+
+if num == 0:
+    result = '0'
+while num > 0:
+    result = str(num%2) + result
+    num = num // 2
+
+print(result)
