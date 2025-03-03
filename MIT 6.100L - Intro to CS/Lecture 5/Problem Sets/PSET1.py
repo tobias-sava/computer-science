891dea96995cad05db38c39e959744147a3bee7b
tobias-sava <tobias.svgb@gmail.com>
2025-03-03 12:13:50 +0200

    push

--- MIT 6.100L - Intro to CS/Lecture 5/Problem Sets/PSET1.py
+++ MIT 6.100L - Intro to CS/Lecture 5/Problem Sets/PSET1.py
@@ -1,4 +1,28 @@
 # Problem Set 0
 # Name: Tobias Sava
-# Date: 26/02/2025
-# Time Spent: X
+# Date: 02/03/2025 (refined it a little on 03/02/2025)
+# Time Spent: 30 minutes
+
+x = 54321
+
+epsilon = 0.01
+
+num_guesses = 0
+
+guess = 0.0
+
+increment = 0.0001
+
+while abs(guess**2 - x) >= epsilon and guess ** 2 <= x:
+    guess += increment
+    num_guesses += 1
+
+print('num_guesses =', num_guesses)
+
+if abs(guess ** 2 - x) >= epsilon:
+    print('Failed on square root of', x)
+else:
+    print(guess, 'is close to square root of', x)
+
+# test cases passed successfully (questioning my existence rn)
+# in conclusion: stay away from floats in order to avoid mental health deterioration
