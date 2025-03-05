c44ea511c2470c5f2f6d287a1c6c6156f5bc848d
tobias-sava <tobias.svgb@gmail.com>
2025-03-05 14:52:01 +0200

    push

---
+++ MIT 6.100L - Intro to CS/Lecture 7/Notes/note2.py
@@ -0,0 +1,30 @@
+# 05/03/2025
+
+def is_even(i): # declaring function, function name, function params (separated by commas if multiple)
+  '''
+  Clear function description so other people can know the purpose of your code.
+  '''
+  if i % 2 == 0: # if i divded by 2's remainder is 0 the number is even
+    return True
+  else:
+    return False
+  
+# Instead of using if statements, we can simply express the equation as it already provides us with
+# a boolean value.
+
+# Instead of:
+
+  if i % 2 == 0: # if i divded by 2's remainder is 0 the number is even
+    return True
+  else:
+    return False
+  
+# 
+
+  return i % 2 == 0
+
+# Will still return True/False based on even/odd.
+
+  
+  
+  
