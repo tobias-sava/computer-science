77d91f8fcbf4d4806cbf686bfb9aca26a47647ab
tobias-sava <tobias.svgb@gmail.com>
2025-03-10 10:14:30 -0700

    finished psets

--- MIT 6.100L - Intro to CS/Lecture 7/Problem Sets/PSET0B.py
+++ MIT 6.100L - Intro to CS/Lecture 7/Problem Sets/PSET0B.py
@@ -13,8 +13,14 @@ def two_quadratics(a1, b1, c1, x1, a2, b2, c2, x2):
     """
     # Your code here
 
+    quad1 = (a1 * (x1 ** 2)) + (b1 * x1) + c1
+    quad2 = (a2 * (x2 ** 2)) + (b2 * x2) + c2
+
+    sum = quad1 + quad2
+
+    print(sum)
+
 # Examples:    
 two_quadratics(1, 1, 1, 1, 1, 1, 1, 1) # prints 6
 print(two_quadratics(1, 1, 1, 1, 1, 1, 1, 1)) # prints 6 then None
 
-# Continue later
