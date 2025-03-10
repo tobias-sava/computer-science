e96f0b6836869e32d689674fd077a1c27c0cc8b5
tobias-sava <tobias.svgb@gmail.com>
2025-03-10 03:14:45 -0700

    continue later

---
+++ MIT 6.100L - Intro to CS/Lecture 7/Problem Sets/PSET0B.py
@@ -0,0 +1,20 @@
+# 10/03/2025
+
+# Question 2: Implement the function that meets the specifications below:
+
+def two_quadratics(a1, b1, c1, x1, a2, b2, c2, x2):
+    """
+    a1, b1, c1: one set of coefficients of a quadratic equation
+    a2, b2, c2: another set of coefficients of a quadratic equation
+    x1, x2: values at which to evaluate the quadratics
+    Evaluates one quadratic with coefficients a1, b1, c1, at x1.
+    Evaluates another quadratic with coefficients a2, b2, c2, at x2.
+    Prints the sum of the two evaluations. Does not return anything.
+    """
+    # Your code here
+
+# Examples:    
+two_quadratics(1, 1, 1, 1, 1, 1, 1, 1) # prints 6
+print(two_quadratics(1, 1, 1, 1, 1, 1, 1, 1)) # prints 6 then None
+
+# Continue later
