01a1dfc8a7af91a148c3274e17327b4239843a81
tobias-sava <tobias.svgb@gmail.com>
2025-03-10 03:13:08 -0700

    solved pset0a

---
+++ MIT 6.100L - Intro to CS/Lecture 7/Problem Sets/PSET0A.py
@@ -0,0 +1,19 @@
+# 10/03/2025
+
+# Implement the function that meets the specifications below:
+
+def eval_quadratic(a, b, c, x):
+    """
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    Returns the value of the quadratic a * x² + b * x + c.
+    """
+    # Your code here
+
+    return (a * x ** 2) + (b * x) + c
+
+
+# Examples:    
+print(eval_quadratic(1, 1, 1, 1)) # prints 3
+
+# Solved
