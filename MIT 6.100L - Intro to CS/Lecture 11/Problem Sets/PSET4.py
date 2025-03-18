7efbb94f2c3d3f899e8994944f510f2686df97ef
tobias-sava <tobias.svgb@gmail.com>
2025-03-18 20:07:01 +0200

    solved pset

--- MIT 6.100L - Intro to CS/Lecture 11/Problem Sets/PSET4.py
+++ MIT 6.100L - Intro to CS/Lecture 11/Problem Sets/PSET4.py
@@ -8,7 +8,6 @@ def remove_and_sort(Lin, k):
         k is an int >= 0
     Mutates Lin to remove the first k element in Lin and
     then sorts the remaining elements in ascending order.
-    If you run out of items to remove, Lin is mutated to an empty list.
     Does not return anything.
     """
     # Your code here
@@ -24,3 +23,5 @@ L = [1,6,3]
 k = 1
 remove_and_sort(L, k)
 print(L)   # prints the list [3, 6]
+
+# Solved.
