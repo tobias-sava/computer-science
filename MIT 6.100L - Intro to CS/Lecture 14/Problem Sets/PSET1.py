29a47e0a4133b6722d459fe174dd30303b324914
tobias-sava <tobias.svgb@gmail.com>
2025-03-23 08:23:18 -0700

    solved pset1

--- MIT 6.100L - Intro to CS/Lecture 14/Problem Sets/PSET1.py
+++ MIT 6.100L - Intro to CS/Lecture 14/Problem Sets/PSET1.py
@@ -1,7 +1,18 @@
 # 22/03/2025
 
-dict = {1:3, 2:3, "penis":21}
+def find_grades(grades, students):
+    """ grades is a dict mapping student names (str) to grades (str)
+        students is a list of student names
+    Returns a list containing the grades for students (in the same order) """
 
-penis = dict["penis"].value()
+    grades_list = []
 
-print(penis)
+    for i in students: # Loops through the list of students we want to get the grades for.
+        grades_list.append(grades[i]) # Appends the value of i key to grades_list.
+
+    return grades_list
+
+# For example:
+
+d = {'Ana':'B', 'Matt':'C', 'John':'B', 'Katy':'A'}
+print(find_grades(d, ['Matt', 'Katy'])) # returns ['C', 'A']
