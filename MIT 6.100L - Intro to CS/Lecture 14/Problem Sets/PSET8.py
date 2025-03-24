2dc31081db1927e23005ad415a2d0917c754f10e
tobias-sava <tobias.svgb@gmail.com>
2025-03-23 17:28:39 -0700

    solved pset8

--- MIT 6.100L - Intro to CS/Lecture 14/Problem Sets/PSET8.py
+++ MIT 6.100L - Intro to CS/Lecture 14/Problem Sets/PSET8.py
@@ -7,6 +7,19 @@ students = {}
 def add_student(students, name):
     students[name] = 0
 
-add_student(students, 'Bob')
+def add_grade(students, name, grade):
+    if name in students:
+        students[name] = grade
+    else:
+        print(f"ERROR: Student {name} not found")
 
+def average_grade(students, name):
+    
+    average = 0
+
+    for i in students[name].values():
+        average = sum(i) / len(i)
+
+add_student(students, 'Bob')
+add_grade(students, 'Tiffany', 10)
 print(students)
