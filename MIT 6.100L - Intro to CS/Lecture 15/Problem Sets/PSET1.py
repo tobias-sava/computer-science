fb360d34808f7bf84c71246b152fe141949d3902
tobias-sava <tobias.svgb@gmail.com>
2025-03-24 05:45:00 -0700

    solved pset1

---
+++ MIT 6.100L - Intro to CS/Lecture 15/Problem Sets/PSET1.py
@@ -0,0 +1,16 @@
+# 23/03/2025
+
+def power_recur(n, p):
+
+    if p == 0:
+        return 1
+    
+    elif p == 1:
+        return n
+    
+    else:
+        return n*power_recur(n, p - 1)
+    
+print(power_recur(3, 4)) # Prints 81
+
+# Solved.
