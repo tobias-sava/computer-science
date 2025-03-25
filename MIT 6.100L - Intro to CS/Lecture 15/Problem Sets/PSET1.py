265344d80a77295fb122ae6528b897b0f8cdbfa6
tobias-sava <tobias.svgb@gmail.com>
2025-03-25 08:55:59 -0700

    solved pset1

--- MIT 6.100L - Intro to CS/Lecture 15/Problem Sets/PSET1.py
+++ MIT 6.100L - Intro to CS/Lecture 15/Problem Sets/PSET1.py
@@ -1,16 +1,22 @@
-# 23/03/2025
+# 25/03/2025
 
-def power_recur(n, p):
+def recur_power(base, exp):
+    """
+    base: int or float.
+    exp: int >= 0
 
-    if p == 0:
+    Returns base to the power of exp using recursion.
+    Hint: Base case is when exp = 0. Otherwise, in the recursive
+    case you return base * base^(exp-1).
+    """
+    # Your code here  
+
+    if exp == 0:
         return 1
-    
-    elif p == 1:
-        return n
-    
     else:
-        return n * power_recur(n, p - 1)
-    
-print(power_recur(3, 4)) # Prints 81
+        return base * recur_power(base, exp - 1)
+
+# Examples:
+print(recur_power(2,5))  # prints 32
 
 # Solved.
