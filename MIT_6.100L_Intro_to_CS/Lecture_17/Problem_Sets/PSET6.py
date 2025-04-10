d8170a363fdac32ae49e416bf43c6359c682f94f
tobias-sava <tobias.svgb@gmail.com>
2025-04-11 00:58:17 +0300

    solved pset6

--- MIT_6.100L_Intro_to_CS/Lecture_17/Problem_Sets/PSET6.py
+++ MIT_6.100L_Intro_to_CS/Lecture_17/Problem_Sets/PSET6.py
@@ -9,11 +9,34 @@ class Pet:
         self.energy = energy
 
     def feed(self, amount):
-        
         self.hunger = max(self.hunger - amount, 0)
-
-        self.hunger -= amount
         self.happiness += 10
 
     def play(self, time):
-        pass
+        self.happiness += time / 2
+        self.energy -= time / 2
+        self.hunger += time / 4
+
+    def sleep(self, hours):
+        self.energy += hours * 30
+        self.hunger += hours * 15
+
+        if hours >= 12: # if sleeping too long
+            self.happiness -= hours * 10
+        else:
+            self.happiness += hours * 2 # increasing happiness by a bit if slept well
+
+    def get_status(self):
+        print(f"{self.name} the {self.species}")
+        print(f"Hunger:    {self.hunger:.1f}")
+        print(f"Happiness: {self.happiness:.1f}")
+        print(f"Energy:    {self.energy:.1f}")
+        print("-" * 30)
+
+Cat_One = Pet("Alisha", "Cat")
+
+Cat_One.sleep(9)
+Cat_One.feed(100)
+Cat_One.play(5)
+
+Cat_One.get_status()
