55838b98e6f1c2491a481a3e9b307682641d7730
tobias-sava <tobias.svgb@gmail.com>
2025-04-30 10:00:16 -0700

    push

--- MIT_6.100L_Intro_to_CS/Lecture_19/Notes/note2.py
+++ MIT_6.100L_Intro_to_CS/Lecture_19/Notes/note2.py
@@ -56,3 +56,55 @@ print(c.get_age()) # prints 5
 # a.speak() # AttributeError: 'Animal' object has no attribute 'speak'
 
 # AttrError because the Animal class does not have the speak method.
+
+
+#
+
+
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self, age) # calling animals init method (sets self.name = name and self.age = age)
+        self.set_name(name) # setting Person object's name
+        self.friends = [] # additional attribute
+    
+    def get_friends(self):
+        return self.friends.copy()
+    
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+    
+    def speak(self)
+        print("hello")
+
+    def age_diff(self, other):
+        diff = self.age - other.age
+        print((abs(diff)), "year difference")
+    
+    def __str__(self):
+        return "person:" + str(self.name) + ":" + str(self.age)
+    
+
+#
+
+
+def make_pets(d):
+    """ d is a dict mapping a Person obj to a Cat obj
+    Prints, on each line, the name of a person,
+    a colon, and the name of that persons's cat """
+
+    d = {}
+
+    for k, v in d.items(): 
+        # k (key) person
+        # v (value) cat
+        print(k.get_name()) + ":" + print(v.get_name())
+
+p1 = Person("ana", 86)
+p2 = Person("james", 7)
+c1 = Cat(1)
+c1.set_name("furball")
+c2 = Cat(1)
+c2.set_name("fluffsphere")
+
+# 56:54
