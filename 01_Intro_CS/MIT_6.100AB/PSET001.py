c5bfd58b3c9e3935e4be8101e93aee09957d58d0
tobias-sava <tobias.svgb@gmail.com>
2025-08-05 11:01:30 +0300

    completed

---
+++ 01_Intro_CS/MIT_6.100AB/PSET001.py
@@ -0,0 +1,18 @@
+# 08/05/2025
+
+# Branching 1
+
+# Assume you're given 2 int variables: n1 and n2. 
+# Print "both" if they are both positive, "only one" if only one of them is positive, and "neither" if neither are positive.
+
+n1 = 0
+n2 = 0
+
+if n1 and n2 >= 1:
+    print("both")
+
+elif n1 or n2 >= 1:
+    print("only one")
+
+else:
+    print("neither")
